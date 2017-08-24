a0577bcb0f5aade6b5d2757c18b92363d281576c
Felix Nehrke <felix.nehrke@di-unternehmer.com>
2017-08-24 18:19:51 +0200

    Implement determineLengthOf and pass the tests

--- determineLengthOf/felix/index.js
+++ determineLengthOf/felix/index.js
@@ -1,1 +1,6 @@
-module.exports = arr => 1
+module.exports = arr => 
+    arr.map(x => x===''+x && x.replace(/,/g,'').replace(/$/, ' ') || 1)
+        .toString()
+	.replace(/^(.)/, ',$1')
+        .split(',')
+        .reduce(y => ++y, -1)
